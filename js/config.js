38143d492a42cf3aca81c3bdb535f837cb06836b
acanosandoval <114962747+acanosandoval@users.noreply.github.com>
2025-03-07 06:21:32 -0600

    ummmm

--- js/config.js
+++ js/config.js
@@ -83,6 +83,84 @@ const config = [
                 timeline_year: 1948,
                 image: './static/images/shelleyvkramer.jpeg',
                 image_credit: 'University of Chicago Photographic Archive'
+            }
+        ]
+    },
+    {
+        chapterTitle:
+            '1949-1962: Urban Renewal and <br>“A Compatible Community”',
+        chapterYears: '1949-1962',
+        id: '3',
+        image: './static/images/chapter3.png',
+        img_credit: 'Hyde Park Herald',
+        subsections: [
+            {
+                id: '3.0',
+                text: `
+                With the outlawing of racial covenants, the Black population in the University’s immediate surroundings continued to increase: Hyde Park had 573 Black residents in 1940 and 1,757 in 1950, most of whom had arrived after 1948, according to Hirsch’s <i>Making the Second Ghetto</i>. Hirsch writes that the University saw maintaining a “desirable” neighborhood as tied to its survival and key to its academic mission. It began substantial property acquisition and “blight removal,” (a term used in the University of Chicago’s 1949 Treasurer’s report) making it the first American higher education institution to embark on a comprehensive urban renewal campaign.`,
+                quote: `"I believe we have found the answer. It is called 'Urban Renewal' ... it is, however, a tragic fact that these efforts are today essential to the ability of the institutions to fulfill their primary responsibilities."`,
+                quoteAuthor:
+                    'Julian Levi, South East Chicago Commission (SECC) executive director and later professor at the University of Chicago',
+                quoteSource: 'Haar, The City as Campus'
+            },
+            {
+                id: '3.0a',
+                text: ``,
+                quote: `"Beginning in the early 1940s, signs of deterioration around the University were evident. As a result of the Depression and the war, many buildings had not been maintained for 15 years, and there had been little new investment in the area... The heavy migration of blacks from the South to Chicago resulted in severe population pressures on the South Side... which led to predatory real estate practices: slumlords illegally converting six flats into twenty-four-unit rooming houses charging exorbitant rental rates, and not maintaining buildings to code."`,
+                quoteAuthor: `Former University Dean and Senior Advisor to the University President John Boyer, <i>The University of Chicago: A History</i>`,
+                quoteSource: ''
+            },
+            {
+                id: '3.1',
+                text: `The South East Chicago Commission (SECC) administered much of Hyde Park’s “urban renewal.” Much of the SECC leadership came from the University; Chancellor Kimpton chose Julian Levi (J.D. ’31) to chair the new Commission. Four main plans comprised Hyde Park’s urban renewal efforts: Hyde Park A + B, the Southwest Hyde Park Redevelopment Corporation Plan, the Hyde Park-Kenwood Urban Renewal Plan, and the South Campus Plan.`,
+                timeline_year: 1949,
+                quote: '"We are fighting for our lives. We simply cannot operate in slums."',
+                quoteAuthor:
+                    'University of Chicago Chancellor Lawrence Kimpton',
+                quoteSource: ''
+            },
+            {
+                id: '3.2',
+                text: `The Hyde Park A + B plan began in 1954. The Land Clearance Commission, a city agency, approved a plan to clear and redevelop approximately 48 acres of land. The intent was to replace “blighted” residences and businesses with groups of similar townhouses along both sides of 55th Street, the twin towers of I. M. Pei’s University Apartments in the middle of 55th Street, and a shopping center at 55th Street and Lake Park Avenue.`,
+                image: './static/images/placeholder.jpg',
+                image_credit:
+                    'View of 55th St from Lake Park Blvd in 1950 and 1961<br>University of Chicago Photographic Archive'
+            },
+            {
+                id: '3.3',
+                text: `
+                Next, the Southwest Hyde Park Redevelopment Corporation used the <a href = "https://chicagounbound.uchicago.edu/cgi/viewcontent.cgi?article=2841&context=uclrev" target = "_blank">Neighborhood Redevelopment Corporation Act</a> to have the city acquire any privately owned land between the campus and Washington Park. Most of that area was marked for “rehabilitation.” The area from 55th to 56th Streets and from Cottage Grove to Ellis Avenues was slated for wholesale demolition and University acquisition.`
+            },
+            {
+                id: '3.4',
+                text: `The largest and most comprehensive phase was the Hyde Park-Kenwood Urban Renewal Plan, drawn up and approved in 1958. The University, through the SECC, was a main plan drafter. The plan focused on both total and “spot” clearance. It slated 638 structures containing 6,147 dwelling units for demolition and called for the creation and modernization of low- and high-density residential areas, parks, schools, residential and commercial areas, and more in their place.`
+            },
+            {
+                id: '3.5',
+                text: `
+                Finally, the University followed through on a suggestion in the 1949 Treasurer’s Report for the University to acquire all property in the strip of land from 60th to 61st Streets and from Cottage Grove to Stony Island Avenues to create a buffer zone between the campus and the “<a href = "https://cegu.uchicago.edu/2023/11/30/university-led-renewal/" target="_blank">deteriorating neighborhood</a>” of Woodlawn immediately to the South. The city purchased everything not yet owned by the University and then sold it to the University in a process that took a decade due to legal disputes and activist groups opposing the acquisition and the expansion. Notably, <a href = "https://www.wttw.com/dusable-to-obama/woodlawn-organization" target = "_blank">The Woodlawn Organization (T.W.O.)</a> successfully prevented the University from expanding further southward than 61st Street.`
+            },
+            {
+                id: '3.5a',
+                text: `Urban renewal fueled unrest across the city. In 1955, Black residents marched on city hall and demanded that Mayor Richard Daley address the discrimination against Black families, writes Eddie R. Cole in <i>The Campus Color Line</i>. Local leaders also raised concerns about the University’s role. “I see this whole area being controlled by a monolithic institution via its political puppets,” accused Wesley W. South, candidate for the 2nd Congressional district in 1968 in the <i>Chicago Defender</i>.`,
+                image: './static/images/ousts.png',
+                image_credit: '<i>Chicago Defender</i>, 1962'
+            },
+            {
+                id: '3.5b',
+                text: ``,
+                quote: `"Land clearance is perverted to Negro Clearance. Urban Renewal, despite its good intentions and its great possibilities, becomes a distortion and a false promise. Slums have not been eradicated but have been made mobile. Most of the new housing units erected, though providing good living, have not provided housing for those displaced."`,
+                quoteAuthor: `Executive Director of the Chicago Urban League Edwin C. Berry, 1962`,
+                quoteSource: ''
+            },
+
+            {
+                id: '3.6',
+                text: `
+                Cumulatively, these plans called for the demolition of 193 acres (20 percent of Hyde Park); cost $120 million ($1.4 billion when adjusted for inflation), approximately half of which was public funds and the other half private; displaced more than 30,000 people; and enabled the University of Chicago to add 41 acres of land to its campus. The campaign was a success, according to the administration. Documented by Levi, it became a model for other institutions, such as <a href = "https://projects.browndailyherald.com/2022/12/13/brown-property-footprint/" target = "_blank">Brown University</a> in the 1960s and <a href = "https://www.theguardian.com/cities/2018/apr/18/gentrify-or-die-inside-a-universitys-controversial-plan-for-baltimore" target = "_blank">Johns Hopkins</a> in the 2010s.`,
+                quote: `"The Chicago Association of Industry Tuesday endorsed a 50-acre redevelopment project for the Hyde Park area. The association said that the project, planned by the South East Chicago Commission, 'may well become a model for similar urban renewal in other sections of the city.'"`,
+                quoteAuthor: '<i>Chicago Daily News</i>, 1954',
+                quoteSource: ''
             },
             {
                 id: 'final-scroller',
