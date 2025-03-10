4c18789457914e1187ab76fb94a77f679dc024a4
acanosandoval <114962747+acanosandoval@users.noreply.github.com>
2025-03-10 05:41:24 -0500

    no chap 5

--- js/config.js
+++ js/config.js
@@ -200,32 +200,6 @@ const config = [
             {
                 id: '4.7',
                 text: `The creation of a private campus shuttle system, an expanded patrol area for UCPD, the EAHP, and the consolidation of student amenities all contributed to creating what Eldred termed “invisible borders” around campus during the second half of the 20th century.`
-            }
-        ]
-    },
-    {
-        chapterTitle: '2007-2022: Expansion and Turning to the City',
-        chapterYears: '2007-2022',
-        id: '5',
-        image: './static/images/chapter5.jpg',
-
-        img_credit: 'UChicago Arts',
-        subsections: [
-            {
-                id: '5.1',
-                text: `
-                In 2006, University President Zimmer made his "<a href = "https://d3qi0qp55mx5f5.cloudfront.net/convocation/docs/zimmer.pdf?mtime=1517338474" target = "_blank">Back to the City</a>" speech, posing the question: “How should our relationship with the South Side community, city, and the region evolve?” In the following years, the University simultaneously sought to shrink its portfolio of residential properties acquired during Urban Renewal and the <a href = "https://chicagomaroon.com/20278/news/univ-to-sell-21-residential-properties-in-hyde-park/" target = "_blank">Great Recession</a> and expand non-residential real estate holdings.`
-            },
-            {
-                id: '5.2',
-                text: `The University <span class="highlight former">sold</span> over 1,200 apartment units and four lots in Hyde Park between 2004 and 2016 and has bought 26 mixed-use properties in Washington Park since 2008. They acquired commercial real estate, such as Harper Court in 2008 and the building now containing Jewel Osco, in 2020. Currently, the University owns the buildings containing four of the major grocery stores in the area.`
-            },
-            {
-                id: '5.3',
-                text: `
-                The University also invested in cultural development throughout the South Side through a partnership with the Hyde Park Arts Center, stewarding the Washington Park <a href = 'https://news.uchicago.edu/story/arts-lawn-community-centered-space-washington-park-celebrates-opening' target = "_blank">Arts Block</a> and opening the <a href = 'https://news.uchicago.edu/story/uchicago-opens-green-line-performing-arts-center-washington-park' target = "_blank">Green Line Performing Arts Center</a> in 2018.`,
-                image: './static/images/arts_block.jpg',
-                image_credit: 'Visual Manifesto'
             },
             {
                 id: 'final-scroller',
